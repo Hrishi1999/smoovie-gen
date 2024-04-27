ed304e8dfc8247d2fbbfbb6aba5ccc436e9476a1
Hrishi <hrishipatel99@gmail.com>
2024-04-26 21:42:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def test():
     return jsonify({'message': 'Hello World!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000, host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
