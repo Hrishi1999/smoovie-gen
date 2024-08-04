be058a664059fcf17deadec2c5a61cda2895dcaf
Hrishi <hrishipatel99@gmail.com>
2024-08-04 18:54:17 -0300

    make changes to post processing after merge

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def process_video(inp, out):
     commands = [
         # './spatial make -i {0} -f ou -o {1} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --primary right --projection rect'.format(inp, out),
         # spatial make -i {inupt_file} -f ou -o {output_file} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --primary right
-        './spatial make -i {0} -f ou -o {1} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --primary right --projection rect'.format(inp, out)
+        './spatial make -i {0} -f ou -o {1} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --primary right --hero right --projection rect --bitrate 200M --quality 1.0'.format(inp, out)
     ]
     for command in commands:
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
