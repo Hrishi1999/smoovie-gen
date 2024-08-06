ec66e88d98b4b71ed162bb80cb464e4e2f13deef
Hrishi <hrishipatel99@gmail.com>
2024-08-06 00:29:12 -0300

    change hero and primary

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def split_video(inp):
 def merge_videos(left_file, right_file, output_file):
     logger.info(f"merging: {left_file} and {right_file}")
 
-    command = f'./spatial make -i {right_file} -i {left_file} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --projection rect --hero left --primary left --bitrate 200M --quality 1.0 -o {output_file}'
+    command = f'./spatial make -i {right_file} -i {left_file} --cdist 19.24 --hfov 63.4 --hadjust 0.02 --projection rect --hero right --primary right --bitrate 200M --quality 1.0 -o {output_file}'
     logger.info(f"executing command: {command}")
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     stdout, stderr = process.communicate()
